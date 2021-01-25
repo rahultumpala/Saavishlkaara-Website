786ce256037bccba66d52df5a289caa647d46dd3
Rahul Tumpala <rhltumpala@gmail.com>
2021-01-25 18:02:52 +0530

    Added more routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,11 +3,11 @@ const viewController = require("../controllers/viewController")
 const router = express.Router();
 
 router.get("/", viewController.getHomepage)
-// router.route("/contact").get(viewController.getContactPage);
-// router.route("/about").get(viewController.getAboutPage);
-// router.route("/blog").get(viewController.getBlogPage);
-// router.route("/our-team").get(viewController.getTeamPage);
-// router.route("/courses").get(viewController.getCoursesPage);
+router.route("/contact").get(viewController.getContactPage);
+router.route("/about").get(viewController.getAboutPage);
+router.route("/blog").get(viewController.getBlogPage);
+router.route("/our-team").get(viewController.getTeamPage);
+router.route("/courses").get(viewController.getCoursesPage);
 
 
 module.exports = router;
