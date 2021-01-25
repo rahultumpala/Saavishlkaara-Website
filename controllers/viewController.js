561624fd3cee5c240e246f49ab0f5c375545c828
Rahul Tumpala <rhltumpala@gmail.com>
2021-01-25 18:32:05 +0530

    Added Route Controllers & Bug Fixes

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -2,10 +2,26 @@ const AppError = require("../utils/AppError");
 const catchAsync = require("../utils/catchAsync");
 const path = require("path")
 
-exports.getContactPage = catchAsync(async (req, res, next) => {
-    res.status(200).render('contact');
+exports.getHomepage = catchAsync(async (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, '../views/index.html'))
 })
 
-exports.getHomepage = catchAsync(async (req,res,next) => {
-    res.status(200).sendFile(path.join(__dirname ,'../views/index.html'))
+exports.getBlogPage = catchAsync(async (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, "../views/blog.html"))
 })
+
+exports.getCoursesPage = catchAsync(async (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, "../views/courses.html"))
+})
+
+exports.getAboutPage = catchAsync(async (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, "../views/about.html"))
+})
+
+exports.getTeamPage = catchAsync(async (req, res, next) => {
+    res.status(200).sendFile(path.join(__dirname, "../views/our-team.html"))
+})
+
+exports.getContactPage = catchAsync(async (req, res, next) => {
+    res.status(200).render("contact");
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const viewController = require("../controllers/viewController")
 const router = express.Router();
 
+router.route("/our-team").get(viewController.getTeamPage);
 router.get("/", viewController.getHomepage)
 router.route("/contact").get(viewController.getContactPage);
 router.route("/about").get(viewController.getAboutPage);
 router.route("/blog").get(viewController.getBlogPage);
-router.route("/our-team").get(viewController.getTeamPage);
 router.route("/courses").get(viewController.getCoursesPage);
 
 
